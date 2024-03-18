230067608e34e7090486e32c1144579e08443dcd
Praveen Kumar <mastermasspk@gmail.com>
2024-03-18 18:37:37 +0530

    Export & requre uses

---
+++ Backend/math.js
@@ -0,0 +1,18 @@
+// exports.sum=(a,b) => a+b;
+// exports.mul=(a,b) => a*b;
+// exports.g=9.8;
+// exports.PI=3.14;
+
+// // module.exports.sum=(a,b) => a+b;
+// // module.exports.mul=(a,b) => a*b;
+// // module.exports.g=9.8;
+// // module.exports.PI=3.14;
+
+// // module.exports ={
+// //     sum:sum,
+// //     mul:mul,
+// //     g:g,
+// //     PI:PI
+
+// // }
+
---
+++ Backend/script.js
@@ -0,0 +1,14 @@
+// let n=5;
+
+// for(let i=0; i<n; i++){
+//     console.log("Hello, ",i);
+// }
+// console.log("Bye");
+
+// let args=process.argv;
+// for(let i=2; i<args.length;i++){
+//     console.log("Hello ",args[i]);
+// }
+
+const info = require("./Fruits");
+console.log(info[0].name);
